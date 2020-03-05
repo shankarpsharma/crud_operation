3a750baf975fd84205767fd048134ed998a1f1f8
shankarpsharma <60914626+shankarpsharma@users.noreply.github.com>
2020-03-05 14:49:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const root=require('./route/route')
 var app=express()
 app.use(bodyParser.json());
 app.use('/users',root);
-
+//Server Started
 app.listen(3001,()=> console.log('Server started at port:3001'));
