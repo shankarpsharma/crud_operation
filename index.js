cb9e41f827be37e0513bbf792bccdca885e92324
shankarpsharma <60914626+shankarpsharma@users.noreply.github.com>
2020-03-05 14:27:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+ 
+const mongoose = require('./database/db');
+const user=require('./models/user');
+const root=require('./route/route')
+
+var app=express()
+app.use(bodyParser.json());
+app.use('/users',root);
+
+app.listen(3001,()=> console.log('Server started at port:3001'));
